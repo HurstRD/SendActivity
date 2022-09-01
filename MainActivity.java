78a3140605916cc00924c471f65ba94f6e98429e
Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>
2022-09-01 10:22:08 -0400

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+import "org.java.lang.*";
+import "org.java.Util.Math";
+import "org.java.lang.ref";
+public static void main(Strings Args[0x*]){
+//!*-- client namespace for automated third party cert authority
+  //NAMESPACE
+  public static void S_Client = "{${!S_Client}? + 0x00.11}";
+  //*--client namespace for sending client
+  //NAMESPACE
+  public static void R_Client = "{${!R_Client}? + 0x11.00}";
+  //*--
+ "0xchcksum for Send Client/\+
+    --*//
+    //CHECKONE
+  public static void Checksum_ = "{#{!CHE|?
+      //*--
+  "0xchcksum for Receiving Client/\+
+    --*//
+  public static void Checksum_ = "{#{!CHE|?
+    //CHECKTWO
+    "0x!()? +/'sh eval | !'\+
+    chcksum Client-Client:Send[]...receiving[]...
+    "0x?|+/\+
+      RECEIVING_SERVER
+   private static Rceiving_Srver
+  "0x00.*+/\+
+      //MAINACTIVITY
+  public class MainActivity extends Activity{
+  
+}
+}
